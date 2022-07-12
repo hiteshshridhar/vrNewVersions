c06ed8b7424118cc716569316f748785e58d1f10
hiteshshridhar <honey7sharma@gmail.com>
2022-07-12 20:51:18 +0200

    changing the sword names and broken blocks

--- Ball_Ninja/Assets/Scripts/VelocityTracker.cs
+++ Ball_Ninja/Assets/Scripts/VelocityTracker.cs
@@ -15,3 +15,24 @@ public class VelocityTracker : MonoBehaviour
         lastFramePos = transform.position;
     }
 }
+
+/*
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityTracker : MonoBehaviour
+{
+    public Transform tracker;
+    public Vector3 velocity;
+
+    private Vector3 lastFramePos;
+
+    void Update()
+    {
+        velocity = (transform.position - lastFramePos) / Time.deltaTime;
+        lastFramePos = transform.position;
+    }
+}
+*/
