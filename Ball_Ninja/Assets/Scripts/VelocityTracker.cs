34f1d2e34e5d149d1aabb9547b321e617545a8c1
hiteshshridhar <honey7sharma@gmail.com>
2022-07-27 16:30:09 +0200

    removed extra files

--- Ball_Ninja/Assets/Scripts/VelocityTracker.cs
+++ Ball_Ninja/Assets/Scripts/VelocityTracker.cs
@@ -17,24 +17,3 @@ public class VelocityTracker : MonoBehaviour
 
     
 }
-
-/*
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class VelocityTracker : MonoBehaviour
-{
-    public Transform tracker;
-    public Vector3 velocity;
-
-    private Vector3 lastFramePos;
-
-    void Update()
-    {
-        velocity = (transform.position - lastFramePos) / Time.deltaTime;
-        lastFramePos = transform.position;
-    }
-}
-*/
